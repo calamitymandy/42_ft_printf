39a672d29e44a5eddee051bd82b5b6e2a227e8e6
Amandine Demuynck <amdemuyn@c1r7s6.42madrid.com>
2022-11-17 19:07:31 +0100

    ok till here

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:50:39 by amdemuyn          #+#    #+#             */
-/*   Updated: 2022/11/17 17:05:23 by amdemuyn         ###   ########.fr       */
+/*   Updated: 2022/11/17 19:03:09 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -19,27 +19,52 @@ void	ft_putchar_len(char c, int *len)
 	*len = *len +1;
 }
 
+void	ft_putnbr_len(int num, int *len)
+{
+	if (num == -2147483648)
+	{
+		write (1, "-2147483648", 11);
+		(*len) += 11;
+		return ;
+	}
+	if (num < 0)
+	{
+		ft_putchar_len('-', len);
+		ft_putnbr_len(num * -1, len);
+	}
+	else
+	{
+		if (num > 9)
+			ft_putnbr_len(num / 10, len);
+		ft_putchar_len(num % 10 + '0', len);
+	}
+}
+
 void	ft_check_type(va_list args, const char str, int *len)
 {
 	if (str == 'c')
 		ft_putchar_len(va_arg(args, int), len);
+	else if (str == '%')
+		ft_putchar_len('%', len);
+	else if (str == 'd' || str == 'i')
+		ft_putnbr_len(va_arg(args, int), len);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
-	int	i;
-	int	len;
+	int		i;
+	int		len;
 
 	i = 0;
 	len = 0;
-	va_start	(args, str);
+	va_start (args, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
 			ft_check_type(args, str[i + 1], &len);
-		   i++;	
+			i++;
 		}
 		else
 		{
@@ -53,5 +78,7 @@ int	ft_printf(const char *str, ...)
 
 int	main()
 {
-	ft_printf("%c", 'a');
+	int test = -2147483648;
+	ft_printf("%d", test);
+	printf("\n%d", test);
 }
