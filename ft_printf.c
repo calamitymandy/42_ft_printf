9bcd959060a6ce94981a13ecf557c6378ddf3ff6
Amandine Demuynck <amdemuyn@c1r9s2.42madrid.com>
2022-11-17 17:34:25 +0100

    20221117

--- ft_printf.c
+++ ft_printf.c
@@ -6,9 +6,24 @@
 /*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:50:39 by amdemuyn          #+#    #+#             */
-/*   Updated: 2022/11/15 17:44:46 by amdemuyn         ###   ########.fr       */
+/*   Updated: 2022/11/17 17:05:23 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void	ft_putchar_len(char c, int *len)
+{
+	write(1, &c, 1);
+	*len = *len +1;
+}
+
+void	ft_check_type(va_list args, const char str, int *len)
+{
+	if (str == 'c')
+		ft_putchar_len(va_arg(args, int), len);
+}
 
 int	ft_printf(const char *str, ...)
 {
@@ -19,19 +34,24 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 	len = 0;
 	va_start	(args, str);
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (!str)
-			return (NULL);
 		if (str[i] == '%')
 		{
-			ft_check_arg(str[i], &args, &len, &i); 
+			ft_check_type(args, str[i + 1], &len);
+		   i++;	
 		}
 		else
 		{
 			ft_putchar_len((char)str[i], &len);
 		}
+		i++;
 	}
 	va_end(args);
 	return (len);
+}
+
+int	main()
+{
+	ft_printf("%c", 'a');
 }
